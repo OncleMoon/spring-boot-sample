c0846f4a81fd41cf27f2049cddc82b2fc37980f2
lihongbo <LiHongBo01@saicmotor.com>
2017-06-21 14:27:56 +0800

    First commit

---
+++ spring-boot-example-hello/src/main/java/com/hongbo/springboot/example/springbootexamplehello/SpringBootExampleHelloApplication.java
@@ -0,0 +1,12 @@
+package com.hongbo.springboot.example.springbootexamplehello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleHelloApplication.class, args);
+	}
+}
---
+++ spring-boot-example-hello/src/test/java/com/hongbo/springboot/example/springbootexamplehello/SpringBootExampleHelloApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hongbo.springboot.example.springbootexamplehello;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootExampleHelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
